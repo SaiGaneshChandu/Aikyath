7ab4a8fcb6b2a7d3851b5d97be16368e0d1c61e8
SaiGaneshChandu <saiganeshchandu012@gmail.com>
2026-03-19 13:24:50 +0530

    fixed allowed hosts

--- festproject/settings.py
+++ festproject/settings.py
@@ -1,41 +1,75 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-aikyath-project-key'
 
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = False   # 🔥 Render lo always False
 
-ROOT_URLCONF = 'festproject.urls'
+ALLOWED_HOSTS = ['*']   # or ['aikyathcreatedbysai.onrender.com']
 
+# ---------------- APPS ----------------
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'festapp',
 ]
 
+# ---------------- MIDDLEWARE ----------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # 🔥 IMPORTANT
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
+# ---------------- URL ----------------
+ROOT_URLCONF = 'festproject.urls'
+
+# ---------------- TEMPLATES ----------------
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,   # 🔥 VERY IMPORTANT
+        'DIRS': [],   # if templates outside, add path
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
-
 ]
+
+# ---------------- DATABASE ----------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# ---------------- STATIC FILES ----------------
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / "festapp/static",
+    os.path.join(BASE_DIR, 'static')
 ]
+
+# WhiteNoise static config
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# ---------------- DEFAULT SETTINGS ----------------
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
