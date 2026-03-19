5146e614216363f0678b1186402dddf52ab71445
SaiGaneshChandu <saiganeshchandu012@gmail.com>
2026-03-19 13:21:10 +0530

    fixed allowed hosts

--- festproject/settings.py
+++ festproject/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aikyath-project-key'
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 ROOT_URLCONF = 'festproject.urls'
 
